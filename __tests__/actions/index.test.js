8daef9ccdeafee4f3cee2b74127f37083f54ea63
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-04-15 18:49:21 -0700

    Add tests for actions and reducers
    Remove unnecessary import

---
+++ __tests__/actions/index.test.js
@@ -0,0 +1,34 @@
+import * as constants from '../../constants';
+import * as actions from '../../actions';
+
+describe('actions', () => {
+  it('should create an action to increment the counter by one', () => {
+    expect(actions.increment()).toEqual({
+      type: constants.INCREMENT
+    });
+  });
+
+  it('should create an action to decrement the counter by one', () => {
+    expect(actions.decrement()).toEqual({
+      type: constants.DECREMENT
+    });
+  });
+
+  it('should create an action to increment the counter by an arbitrary amount', () => {
+    expect(actions.incrementBy(707)).toEqual({
+      type: constants.INCREMENT_BY,
+      payload: {
+        number: 707
+      }
+    });
+  });
+
+  it('should create an action to decrement the counter by an arbitrary amount', () => {
+    expect(actions.decrementBy(858)).toEqual({
+      type: constants.DECREMENT_BY,
+      payload: {
+        number: 858
+      }
+    });
+  });
+});
